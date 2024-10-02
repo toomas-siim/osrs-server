d90dafe8a8bdb7a3443a28cb1d573b1aff84beae
Toomas-siim Teresk <siim.teresk@gmail.com>
2024-10-02 17:36:24 +0300

    script changes

--- Server/src/main/java/Server/plugin/ai/general/scriptrepository/SimpleLogger.kt
+++ Server/src/main/java/Server/plugin/ai/general/scriptrepository/SimpleLogger.kt
@@ -12,10 +12,11 @@ import plugin.ai.skillingbot.SkillingBotAssembler
 @ScriptIdentifier("simple_logger")
 class SimpleLogger() : Script() {
 
-    private val loggedIds = mutableSetOf<Int>() // Set to store unique IDs
+    private val loggedIds // Set to store unique IDs
 
     override fun tick() {
         SystemLogger.log("Tick invoked: Logging unique nearby entities.")
+        loggedIds = mutableSetOf<Int>()
 
         logUniqueNearbyNodes()
     }
