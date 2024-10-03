9d7ea9c302630199c7bdd44ef5efd57053fc2919
Toomas-siim Teresk <siim.teresk@gmail.com>
2024-10-03 13:13:48 +0300

    script changes

--- Server/src/main/java/Server/plugin/ai/general/scriptrepository/CathShark.kt
+++ Server/src/main/java/Server/plugin/ai/general/scriptrepository/CathShark.kt
@@ -82,10 +82,7 @@ class CathShark : Script() {
                     SystemLogger.log("No fishing spot found. State changed to IDLE")
                 } else {
                     SystemLogger.log("Possible interactions with fishing spot:")
-                    for (i in spot.interaction.getOptions()) {
-                    	SystemLogger.log("Interaction ${i}")
-                    }
-                    spot.interaction.handle(bot, spot.interaction[0])
+                    spot.interaction.handle(bot, spot.interaction[2])
                     SystemLogger.log("Interacting with fishing spot using interaction 0.")
                 }
 
